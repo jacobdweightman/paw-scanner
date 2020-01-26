5f5055d313c73aef12aa08eb057f5b47c1ef7ec3
csub23 <45542177+csub23@users.noreply.github.com>
2020-01-26 09:31:20 -0600

    adding html

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { getDog, getDogs } = require('./database')
 const app = express()
 const port = 9001
 
+app.use('/', express.static(__dirname + '/assets'));
+
 app.get("/", function(req, res) {
     res.send("Hello world!");
 });
