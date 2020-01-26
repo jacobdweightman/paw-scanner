52b550ef3879ae0010a2371ff923535aa0411d43
jacobdweightman <jacobdweightman@gmail.com>
2020-01-26 11:22:27 -0600

    Add deployment file for gcloud

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getDog, getDogs } = require('./database')
 
 const app = express()
-const port = 9001
+const port = 8080
 
 app.use('/', express.static(__dirname + '/assets'));
 
